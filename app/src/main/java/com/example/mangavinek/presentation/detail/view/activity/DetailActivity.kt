912d0a84cbe4e76c07274921bedb1c19afc133ff
charlesmoreira <Onre1515>
2019-06-08 21:01:32 -0300

    Update layout, removido adapter.

--- app/src/main/java/com/example/mangavinek/presentation/detail/view/activity/DetailActivity.kt
+++ app/src/main/java/com/example/mangavinek/presentation/detail/view/activity/DetailActivity.kt
@@ -4,21 +4,17 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import androidx.recyclerview.widget.GridLayoutManager
 import com.bumptech.glide.Glide
 import com.example.mangavinek.R
 import com.example.mangavinek.core.constant.Constant
 import com.example.mangavinek.core.util.Resource
 import com.example.mangavinek.model.detail.entity.DetailResponse
-import com.example.mangavinek.presentation.detail.view.adapter.ItemAdapter
 import com.example.mangavinek.presentation.detail.view.viewmodel.DetailViewModel
 import kotlinx.android.synthetic.main.activity_detail.*
 import org.jetbrains.anko.AnkoLogger
-import org.jetbrains.anko.info
 import org.jsoup.select.Elements
 
 class DetailActivity : AppCompatActivity(), AnkoLogger {
-    private lateinit var adapter: ItemAdapter
     private val detailViewModel: DetailViewModel by lazy {
         ViewModelProviders.of(this).get(DetailViewModel::class.java)
     }
@@ -48,15 +44,6 @@ class DetailActivity : AppCompatActivity(), AnkoLogger {
                         Glide.with(this@DetailActivity)
                             .load(Constant.BASE_URL.plus(value.image))
                             .into(image_cover)
-
-                        val partsAvailable = value.issueAvailable.split(" ".toRegex())
-                        val partsTranslated = value.issueTranslated.split(" ".toRegex())
-                        val partsUnavailable = value.issueUnavailable.split(" ".toRegex())
-
-                        val listCompleted = (partsAvailable + partsTranslated + partsUnavailable)
-
-                        info { listCompleted }
-                        initRecycler(listCompleted)
                     }
                 }
                 is Resource.Error -> {
@@ -64,13 +51,5 @@ class DetailActivity : AppCompatActivity(), AnkoLogger {
             }
         })
 
-    }
-
-    fun initRecycler(parts: List<String>) {
-        adapter = ItemAdapter(parts, this)
-        recycler_issue.adapter = adapter
-        val gridLayoutManager = GridLayoutManager(this, 3)
-        recycler_issue.layoutManager = gridLayoutManager
-        adapter.notifyDataSetChanged()
     }
 }
