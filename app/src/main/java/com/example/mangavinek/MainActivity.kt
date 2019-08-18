7e5a41c5f82c3569e1842314ffd5402702de1b09
charlesmoreira <Onre1515>
2019-08-17 23:41:36 -0300

    :rocket:

--- app/src/main/java/com/example/mangavinek/MainActivity.kt
+++ app/src/main/java/com/example/mangavinek/MainActivity.kt
@@ -12,7 +12,7 @@ import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
