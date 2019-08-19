d8cbb0f40c5c8a8926d76638b7314583e8d9b3fe
charlesmoreira <charlesmoreira@brq.com>
2019-08-19 16:44:16 -0300

    :rocket:

--- app/src/main/java/com/example/mangavinek/core/service/ApiClientSoup.kt
+++ app/src/main/java/com/example/mangavinek/core/service/ApiClientSoup.kt
@@ -1,21 +1,19 @@
 package com.example.mangavinek.core.service
 
+import com.example.mangavinek.BuildConfig
 import org.jsoup.Connection
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 
 object JSoupService {
-    private const val LOGIN_ACTION_URL = "http://soquadrinhos.com/member.php"
-    private const val USERNAME = "oliverdamon"
-    private const val PASSWORD = "onrezzero"
 
     fun getApiClient(url: String): Document {
-        val response = Jsoup.connect(LOGIN_ACTION_URL)
+        val response = Jsoup.connect(BuildConfig.LOGIN_ACTION_URL)
             .method(Connection.Method.POST)
             .data("action", "do_login")
             .data("url", "/portal.php")
-            .data("username", USERNAME)
-            .data("password", PASSWORD)
+            .data("username", BuildConfig.USERNAME)
+            .data("password", BuildConfig.PASSWORD)
             .data("loginsubmit", "Entrar")
             .execute()
 
